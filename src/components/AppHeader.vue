ad95eed89a1b76e56e76fd11ddaa3db348014a64
razibmahmud50 <razibmahmud50@gmail.com>
2020-03-26 22:16:14 +0600

    add header added

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="app-header ui secondary pointing menu">
+  <a class="item">
+    Home
+  </a>
+  <a class="item">
+    Messages
+  </a>
+  <a class="item active">
+    Friends
+  </a>
+  <div class="right menu">
+    <a class="ui item">
+      Logout
+    </a>
+  </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<style>
+
+</style>
